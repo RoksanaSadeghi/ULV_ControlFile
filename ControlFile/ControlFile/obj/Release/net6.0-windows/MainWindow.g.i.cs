20b4405a79063281c6a229d94a1245cb9f7f5abd
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2023-10-05 12:49:38 -0700

    git version

--- ControlFile/ControlFile/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ ControlFile/ControlFile/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -134,7 +134,7 @@ namespace ControlFile {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ControlFile;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ControlFile;V2.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
